9d2425663e5bf8dd3dcef4aa9f46f48db22eace8
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2019-04-08 17:02:46 -0300

    Switch unsupported config to pytest option

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-import pkg_resources
+import shlex
 
 import nengo.conftest
 from nengo.conftest import seed, rng  # pylint: disable=unused-import
@@ -8,6 +8,17 @@ import pytest
 import tensorflow as tf
 
 from nengo_dl import config, simulator, utils
+
+
+def pytest_configure(config):
+    # join all the lines and then split (preserving quoted strings)
+    unsupported = shlex.split(
+        " ".join(config.getini("nengo_test_unsupported")))
+    # group pairs (representing testname + reason)
+    unsupported = [
+        unsupported[i:i + 2] for i in range(0, len(unsupported), 2)]
+
+    simulator.Simulator.unsupported = unsupported
 
 
 def pytest_runtest_setup(item):
@@ -31,9 +42,15 @@ def pytest_addoption(parser):
                      choices=("float32", "float64"),
                      help="Simulator float precision")
     parser.addoption("--unroll-simulation", default=1, type=int,
-                     help="unroll_simulation value for Simulator")
+                     help="`unroll_simulation` parameter for Simulator")
     parser.addoption("--device", default=None,
-                     help="device parameter for Simulator")
+                     help="`device` parameter for Simulator")
+
+    if nengo.version.version_info <= (2, 8, 0):
+        # add the pytest option from future nengo versions
+        parser.addini("nengo_test_unsupported", type="linelist",
+                      help="List of unsupported unit tests with reason for "
+                           "exclusion")
 
 
 @pytest.fixture(scope="session")
@@ -48,8 +65,6 @@ def Simulator(request):
     inference_only = request.config.getoption("--inference-only")
 
     def TestSimulator(net, *args, **kwargs):
-        # raise Exception
-
         kwargs.setdefault("unroll_simulation", unroll)
         kwargs.setdefault("device", device)
 
